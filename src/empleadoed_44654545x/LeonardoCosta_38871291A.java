ac8654c86a3ac133866a5c9b05e03039545f9efc
LEONARDO COSTA <asdjklaf@amsmails.com>
2022-01-17 16:11:54 +0100

    version final con javadoc DNI 38871291A

--- src/empleadoed_44654545x/LeonardoCosta_38871291A.java
+++ src/empleadoed_44654545x/LeonardoCosta_38871291A.java
@@ -5,19 +5,38 @@
 package empleadoed_44654545x;
 
 /**
- *
- * @author cash
+ *EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+ * @author Leonardo Costa DNI 38871291A
+ * @version 1.0
  */
 import java.util.*;
-// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
+
+/**
+ * Constructor con tres parametros
+ */
 public class LeonardoCosta_38871291A {
  static final double FACTOR_HORA_EXTRA = 1.2;
  private String nombreCompleto, dni; 
  private double salarioBasePorHora = 10.56; 
+ /**
+  * 
+  * @param nombreCompleto Nombre completo en mayusculas
+  * @param dni documento nacional de identidad con letra y sin espacios
+  */
  public LeonardoCosta_38871291A(String nombreCompleto, String dni) {
  this.nombreCompleto = nombreCompleto;
  this.dni = dni; 
  } 
+ /**
+  * Calcula el salario bruto mensual en función del salario base por hora
+  * y las horas extras.
+  * 
+  * El precio por hora de la hora extra se determina con la
+  * constante FACTOR_HORA_EXTRA
+  * 
+  * @param horasExtra número de horas extra dedicadas redondeando a la baja(entero)
+  * @return el salario bruto mensual en euros con dos decimales.
+  */
  public double getSalarioEsteMes(int horasExtra) {
  double cantidadExtra, salarioFinal;
  cantidadExtra = horasExtra * this.salarioBasePorHora * 
